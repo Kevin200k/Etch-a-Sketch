e846d87a9065cbab8c00aaef6b41618698be835e
Michael Amos <mikimiki2194@gmail.com>
2024-01-19 21:22:49 +0100

    javascript.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,8 @@
 let container = document.querySelector(".container");
 for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){ 
+    for(let j = 0; j < 16; j++){
          let div = document.createElement("div");
-        container.appendChild(div);
-
-    }
-}
-
-container.style.cssText = "backround-color: black; display: flex; flex-wrap: wrap; "
-let div = document.querySelector("div")
-div.addEventListener("click",function(){
-    div.style.backGroundColor = "black";
-});
+         container.appendChild(div);
+     }};
+container.style.cssText = "display: flex; flex-wrap:wrap; ";
+    
